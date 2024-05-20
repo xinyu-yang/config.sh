77960f74b0041719f280a89141a23fc29320ca16
yxy@dthub <yangxinyu75@live.com>
2024-05-20 13:57:02 +0000

    add alias for copilot

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -23,3 +23,22 @@ alias ll='ls -lh --color=auto'
 # Add PATH
 #fish_add_path -m ~/.local/bin
 contains ~/.local/bin $fish_user_paths; or set -xp fish_user_paths ~/.local/bin
+
+# Add alias for gh copilot
+alias ghcs "gh copilot suggest"
+alias ghce "gh copilot explain"
+
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /home/xinyu/miniforge3/bin/conda
+    eval /home/xinyu/miniforge3/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/home/xinyu/miniforge3/etc/fish/conf.d/conda.fish"
+        . "/home/xinyu/miniforge3/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/home/xinyu/miniforge3/bin" $PATH
+    end
+end
+# <<< conda initialize <<<
+
